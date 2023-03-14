ccb2c71e56ceaae7119dd6994379d42bc9436789
André Carlos Heringer <70924249+andrejr105@users.noreply.github.com>
2023-03-14 16:46:52 -0300

    Implementação da Camada Dal
    Nessa etapa foram implementadas as classes UsuarioDal, PermissaoDal e GrupoUsuarioDal.

---
+++ Configuracao/DAL/PermissaoDal.cs
@@ -0,0 +1,40 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class PermissaoDal
+    {
+        public void Iserir(Permissao _permissao)
+        {
+
+        }
+        public List<Permissao> BuscarTodos()
+        {
+
+            throw new NotImplementedException();
+        }
+        public List<Permissao> BuscarPorDescricao(string _descricao)
+        {
+
+            throw new NotImplementedException();
+        }
+        public List<Permissao> BuscarPorId(int _id)
+        {
+
+            throw new NotImplementedException();
+        }
+        public void Alterar(Permissao _permissao)
+        {
+
+        }
+        public void Excluir(int _id)
+        {
+
+        }
+    }
+}
---
+++ Configuracao/DAL/UsuarioDal.cs
@@ -0,0 +1,40 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class UsuarioDal
+    {
+        public void Iserir(Usuario _usuario)
+        {
+
+        }
+        public List<Usuario> BuscarTodos()
+        {
+
+            throw new NotImplementedException();
+        }
+        public List<Usuario> BuscarPorNomeUsuario(string _nomeUsuario)
+        {
+
+            throw new NotImplementedException();
+        }
+        public List<Usuario> BuscarPorId(int _id)
+        {
+
+            throw new NotImplementedException();
+        }
+        public void Alterar(Usuario _usuario)
+        {
+
+        }
+        public void Excluir(int _id)
+        {
+
+        }
+    }
+}
